6972de4407ae3b7bac6b5d6e509b9b3035be501c
Shulamit <s0548411104@gmail.com>
2026-09-08 21:48:10 +0300

    Publish web application with GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -17,7 +17,7 @@ export const appConfig: ApplicationConfig = {
 
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     // איחוד הקריאות ל-HttpClient - חשוב מאוד!
     provideHttpClient(withInterceptors([authInterceptor])), 
     provideAnimations() 
